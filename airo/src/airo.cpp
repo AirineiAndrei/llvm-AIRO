aa244824172239a17c6cc8c6eadeb75a63fc0be5
Airinei Andrei <acairinei17@gmail.com>
2024-02-23 06:37:48 +0200

    wip ipc

--- airo/src/airo.cpp
+++ airo/src/airo.cpp
@@ -1,12 +1,21 @@
 #include "airo.hpp"
+#include "ipc.hpp"
 #include <iostream>
 
 namespace airo{
     std::optional<int> unroll_hook(llvm::Loop *L)
     {
-        std::cout<<"Hello";
+        std::unique_ptr<IPC> ipc;
+        try
+        {
+            ipc = std::make_unique<IPC>();
+        }catch(...)
+        {
+            std::cout<<"error on pipe"<<std::endl;
+        }
+        int ans = ipc->send("Hello from clang");
         std::cout << "Loop depth " << L->getLoopDepth() << std::endl;
-        return 20;
+        return ans;
     }
 }
 
