503d87cfccdd456cfe6bcfdc0a3fc99db8dac385
Airinei Andrei <acairinei17@gmail.com>
2024-04-07 17:48:12 +0300

    Serialize loop info add nlohmann/json.hpp

--- airo/src/airo.cpp
+++ airo/src/airo.cpp
@@ -1,14 +1,52 @@
 #include "airo.hpp"
 #include "ipc.hpp"
 #include <iostream>
+#include "nlohmann/json.hpp"
+
+using json = nlohmann::json;
 
 namespace airo{
-    std::optional<int> unroll_hook(llvm::Loop *L)
-    {
-        std::cout << "Unroll hook" << std::endl;
-        // TODO: seriaaalize loop
-        auto ans = IPC::getInstance()->send("looooooooooooooop");
-        return ans;
-    }
-}
+    std::optional<int> unroll_hook(llvm::Loop *L) {
+    if (!L) return std::nullopt;
 
+    json loop;
+    if (auto *header = L->getHeader()) {
+        loop["header"] = {{"name", header->getName().str()},
+                          {"numInstructions", std::distance(header->begin(), header->end())}};
+    }
+    if (auto *latch = L->getLoopLatch()) {
+        loop["latch"] = {{"name", latch->getName().str()}};
+    }
+    loop["depth"] = L->getLoopDepth();
+    loop["isInnermost"] = L->isInnermost();
+    json blocksJson;
+    for (auto *BB : L->blocks()) {
+        if (BB) {
+            json block = {{"name", BB->getName().str()},
+                          {"numInstructions", std::distance(BB->begin(), BB->end())}};
+            json preds, succs;
+            for (auto *Pred : predecessors(BB)) {
+                preds.push_back(Pred->getName().str());
+            }
+            for (auto *Succ : successors(BB)) {
+                succs.push_back(Succ->getName().str());
+            }
+            block["predecessors"] = preds;
+            block["successors"] = succs;
+            blocksJson.push_back(block);
+        }
+    }
+    loop["blocks"] = blocksJson;
+
+    json subloops;
+    for (auto *SL : *L) {
+        json subloop = {{"header", SL->getHeader()->getName().str()},
+                        {"depth", SL->getLoopDepth()}};
+        subloops.push_back(subloop);
+    }
+    loop["subloops"] = subloops;
+    
+    auto ans = IPC::getInstance()->send(loop.dump());
+    return ans;
+}
+}
